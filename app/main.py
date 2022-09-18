77c868cb43483ddd65f0adcbb5955d22fd912136
Jonny-programmer <76569574+Jonny-programmer@users.noreply.github.com>
2022-09-18 11:47:44 +0300

    Added flask

--- app/main.py
+++ app/main.py
@@ -1,20 +1,28 @@
+from flask import Flask
+from asgiref.wsgi import WsgiToAsgi
+from flask.logging import default_handler
 from fastapi import Depends, FastAPI
 # Можно вместо этого absolute import: from app.routers import items, users
+from flask import render_template
+
 from app.db import database
 from .dependencies import get_token_header
 from .internal import admin
 from .routers import items, users
 from app.config import *
 
-app = FastAPI(dependencies=[Depends(get_token_header)])
+app = Flask(__name__)
+app.logger.removeHandler(default_handler)
+
+api = FastAPI(dependencies=[Depends(get_token_header)])
 
 database.global_init(SQLALCHEMY_DATABASE_URI)
 
-app.include_router(users.router)
-app.include_router(items.router)
+api.include_router(users.router)
+api.include_router(items.router)
 # You can add the same router multiple times with different prefixes This might be useful, for example, when you want
 # to expose API under different prefixes, e.g.: /api/v1 and /api/latest
-app.include_router(
+api.include_router(
     admin.router,
     prefix="/admin",
     tags=["admin"],
@@ -23,6 +31,14 @@ app.include_router(
 )
 
 
-@app.get("/")
+wsgi = WsgiToAsgi(app)
+
+
+@api.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    return {'message': 'Hello from the fastapi application!!'}
+
+
+@app.route("/")
+def root():
+    return render_template('index.html')
---
+++ main.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == '__main__':
+    # os.system("pip install -r requirements.txt")
+    os.system("cd app")
+    os.system("uvicorn app.main:wsgi --reload")
